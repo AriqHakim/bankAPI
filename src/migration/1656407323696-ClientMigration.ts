6e83ce6118a6198e14aaa3e0a2aa1063fe5845bc
AriqHakim <ariq.hakim2002@gmail.com>
2022-06-30 16:13:01 +0700

    -add migration -update env and dependencies

--- src/migration/1656407323696-ClientMigration.ts
+++ src/migration/1656407323696-ClientMigration.ts
@@ -4,7 +4,7 @@ export class ClientMigration1656407323696 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "teller",
+        name: "client",
         columns: [
           {
             name: "id",
@@ -12,17 +12,62 @@ export class ClientMigration1656407323696 implements MigrationInterface {
             isPrimary: true,
           },
           {
-            name: "name",
+            name: "first_name",
             type: "varchar",
           },
           {
-            name: "teller_number",
+            name: "last_name",
             type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "card-number",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "balance",
+            type: "numeric",
+          },
+          {
+            name: "active",
+            type: "boolean",
+            default: "true",
+          },
+          {
+            name: "additional_info",
+            type: "simple-json",
+            // value: {
+            //   age: "number",
+            //   hair_color: "string";
+            // },
+            isNullable: true,
+          },
+          {
+            name: "family_member",
+            type: "string",
+            isArray: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
           },
         ],
       })
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("teller");
+  }
 }
